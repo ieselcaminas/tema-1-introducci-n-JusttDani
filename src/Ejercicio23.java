d1194e8fb6f7623cb2e64343f29b84207a0e4264
Dani Tejedor <dantejmar@alu.edu.gva.es>
2024-09-20 12:36:21 +0200

    Ejercicio23

---
+++ src/Ejercicio23.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejercicio23 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int positivo = 1;
+        int numero = 1;
+        while(numero != 0){
+            numero = sc.nextInt();
+            if(numero > 0){
+                positivo++;
+            }
+        }
+        System.out.println("Hay " + positivo+" positivos");
+
+    }
+}
