69179d5475f98af8bba493b7e0f058cd732eec02
Dani Tejedor <dantejmar@alu.edu.gva.es>
2024-09-20 12:18:06 +0200

    Ejercicio22

---
+++ src/Ejercicio22.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ejercicio22 {
+    public static void main(String[] args) {
+        Scanner inputValue = new Scanner(System.in);
+        int numero;
+        int positivos  = 0;
+        for( int i = 0; i < 10; i++ ){
+            numero = inputValue.nextInt();
+            if(numero > 0){
+                positivos++;
+            }
+        }
+        System.out.println("Hay " + positivos + " positivos");
+    }
+}
